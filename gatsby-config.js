08b0ed72d2b4d257a99bc1e8caf53c919a7afd91
J.C. Hiatt <jc@jchiatt.com>
2019-04-25 12:10:11 -0500

    update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `J.C. Hiatt`,
+    author: `J.C. Hiatt`,
+    description: `I build stuff for screens and lift heavy objects.`,
+    siteUrl: `https://jchiatt.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `jchiatt`,
     },
   },
   plugins: [
