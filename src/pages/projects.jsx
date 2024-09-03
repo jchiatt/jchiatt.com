9d532a77f7dbb0b9c6bc7254b2805ab419ba9736
J.C. Hiatt <jc@jchiatt.com>
2024-09-03 17:04:39 -0500

    update projects page language

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -82,13 +82,10 @@ export default function Projects() {
     <>
       <Head>
         <title>Projects - J.C. Hiatt</title>
-        <meta
-          name="description"
-          content="Things I've made trying to put my dent in the universe."
-        />
+        <meta name="description" content="Here's a few things I've done." />
       </Head>
       <SimpleLayout
-        title="Things I've made trying to put my dent in the universe."
+        title="Here's a few things I've done."
         intro="Out of the projects I've worked on that I am allowed to talk about, here are the ones I'm most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
       >
         <ul
